e965f6dc1a477400734018e7effc6e2ac24d9909
somhi <somhix@gmail.com>
2023-10-04 18:48:13 +0200

    zxtres top formatting

--- modules/jtframe/target/zx3a200/jtframe_zxtres_top.vhd
+++ modules/jtframe/target/zx3a200/jtframe_zxtres_top.vhd
@@ -9,22 +9,22 @@ use work.demistify_config_pkg.all;
 
 entity jtframe_zxtres_top is
 	port (
-		CLK_50     : in std_logic;
+		CLK_50      : in std_logic;
 
-		LED5       : out std_logic := '1';
-		LED6       : out std_logic := '1';
+		LED5        : out std_logic := '1';
+		LED6        : out std_logic := '1';
 		-- SDRAM
-		DRAM_CLK   : out std_logic;
-		DRAM_CKE   : out std_logic;
-		DRAM_ADDR  : out std_logic_vector(12 downto 0);
-		DRAM_BA    : out std_logic_vector(1 downto 0);
-		DRAM_DQ    : inout std_logic_vector(15 downto 0);
-		DRAM_LDQM  : out std_logic;
-		DRAM_UDQM  : out std_logic;
-		DRAM_CS_N  : out std_logic;
-		DRAM_WE_N  : out std_logic;
-		DRAM_CAS_N : out std_logic;
-		DRAM_RAS_N : out std_logic;
+		DRAM_CLK    : out std_logic;
+		DRAM_CKE    : out std_logic;
+		DRAM_ADDR   : out std_logic_vector(12 downto 0);
+		DRAM_BA     : out std_logic_vector(1 downto 0);
+		DRAM_DQ     : inout std_logic_vector(15 downto 0);
+		DRAM_LDQM   : out std_logic;
+		DRAM_UDQM   : out std_logic;
+		DRAM_CS_N   : out std_logic;
+		DRAM_WE_N   : out std_logic;
+		DRAM_CAS_N  : out std_logic;
+		DRAM_RAS_N  : out std_logic;
 		-- SRAM
 		SRAM_A      : out   std_logic_vector(19 downto 0) := (others => '0');
 		SRAM_Q      : inout std_logic_vector(15 downto 0) := (others => 'Z');
@@ -33,11 +33,11 @@ entity jtframe_zxtres_top is
 		SRAM_UB     : out   std_logic := '1';
 		SRAM_LB     : out   std_logic := '1';
 		-- VGA
-		VGA_HS : out std_logic;
-		VGA_VS : out std_logic;
-		VGA_R  : out std_logic_vector(7 downto 0);
-		VGA_G  : out std_logic_vector(7 downto 0);
-		VGA_B  : out std_logic_vector(7 downto 0);
+		VGA_HS 		: out std_logic;
+		VGA_VS 		: out std_logic;
+		VGA_R  		: out std_logic_vector(7 downto 0);
+		VGA_G  		: out std_logic_vector(7 downto 0);
+		VGA_B  		: out std_logic_vector(7 downto 0);
 		-- EAR
 		-- EAR 			 : in std_logic;
 		-- PS2
@@ -46,27 +46,27 @@ entity jtframe_zxtres_top is
 		PS2_MOUSE_CLK    : inout std_logic;
 		PS2_MOUSE_DAT    : inout std_logic;
 		-- UART
-		PMOD4_D4 : in std_logic;		--UART_RXD
-		PMOD4_D5 : out std_logic;		--UART_TXD
-		PMOD4_D6 : in std_logic;		--UART_CTS
-		PMOD4_D7 : out std_logic;		--UART_RTS		
+		PMOD4_D4 	: in std_logic;		--UART_RXD
+		PMOD4_D5 	: out std_logic;		--UART_TXD
+		PMOD4_D6 	: in std_logic;		--UART_CTS
+		PMOD4_D7 	: out std_logic;		--UART_RTS		
 		-- JOYSTICK
-        joy_clk : out std_logic;
-        joy_load_n : out std_logic;
-        joy_data : in std_logic;
-        joy_sel : out std_logic := '1';
+        joy_clk		: out std_logic;
+        joy_load_n	: out std_logic;
+        joy_data	: in std_logic;
+        joy_sel		: out std_logic := '1';
 		-- SD Card
 		SD_CS_N_O   : out std_logic := '1';
 		SD_SCLK_O   : out std_logic := '0';
 		SD_MOSI_O   : out std_logic := '0';
 		SD_MISO_I   : in std_logic;
 		-- I2S audio		
-		I2S_BCLK   : out std_logic := '0';
-		I2S_LRCLK  : out std_logic := '0';
-		I2S_DATA   : out std_logic := '0';
+		I2S_BCLK    : out std_logic := '0';
+		I2S_LRCLK   : out std_logic := '0';
+		I2S_DATA    : out std_logic := '0';
 		-- PWM AUDIO
-		PWM_AUDIO_L 	: out std_logic;
-		PWM_AUDIO_R 	: out std_logic
+		PWM_AUDIO_L : out std_logic;
+		PWM_AUDIO_R : out std_logic
 	);
 END entity;
 
@@ -180,15 +180,15 @@ architecture RTL of jtframe_zxtres_top is
 		);
 	end component;
 
-	signal act_led : std_logic;
-	signal osd_en   : std_logic;
-	signal lf_sram : std_logic;
+	signal act_led 	  : std_logic;
+	signal osd_en  	  : std_logic;
+	signal lf_sram 	  : std_logic;
 
 	signal CLK_50_buf : std_logic;
 	
-	alias clock_input 	: std_logic is CLK_50;
-	alias sigma_l : std_logic is PWM_AUDIO_L;
-	alias sigma_r : std_logic is PWM_AUDIO_R;
+	alias clock_input : std_logic is CLK_50;
+	alias sigma_l 	  : std_logic is PWM_AUDIO_L;
+	alias sigma_r 	  : std_logic is PWM_AUDIO_R;
 
 begin
 
@@ -237,12 +237,12 @@ joy : component joydecoder_neptuno
 		joy2_fire2_o  => joy2fire2
 	);
 	
-joya        <= "11" & joy1fire2 & joy1fire1 & joy1right & joy1left & joy1down & joy1up;
-joyb        <= "11" & joy2fire2 & joy2fire1 & joy2right & joy2left & joy2down & joy2up;
+joya <= "11" & joy1fire2 & joy1fire1 & joy1right & joy1left & joy1down & joy1up;
+joyb <= "11" & joy2fire2 & joy2fire1 & joy2right & joy2left & joy2down & joy2up;
 
 -- Core direct joystick
-joy1                <= joy1fire2 & joy1fire1 & joy1up & joy1down & joy1left & joy1right;
-joy2                <= joy2fire2 & joy2fire1 & joy2up & joy2down & joy2left & joy2right;
+joy1 <= joy1fire2 & joy1fire1 & joy1up & joy1down & joy1left & joy1right;
+joy2 <= joy2fire2 & joy2fire1 & joy2up & joy2down & joy2left & joy2right;
 
 --  Joystick intercept signal
 process(CLK_50_buf)
@@ -270,138 +270,137 @@ audio_i2s : entity work.audio_top
 	);
 
 
+guest : component mist_top
+	port map
+	(
+		CLOCK_27 	=> clock_input&clock_input,
+		CLOCK_27_buff => CLK_50_buf,
+		LED 		=> act_led,
 
-	guest : component mist_top
-		port map
-		(
-			CLOCK_27 	=> clock_input&clock_input,
-			CLOCK_27_buff => CLK_50_buf,
-			LED 		=> act_led,
+		--SDRAM
+		SDRAM_DQ   => DRAM_DQ,
+		SDRAM_A    => DRAM_ADDR,
+		SDRAM_DQML => DRAM_LDQM,
+		SDRAM_DQMH => DRAM_UDQM,
+		SDRAM_nWE  => DRAM_WE_N,
+		SDRAM_nCAS => DRAM_CAS_N,
+		SDRAM_nRAS => DRAM_RAS_N,
+		SDRAM_nCS  => DRAM_CS_N,
+		SDRAM_BA   => DRAM_BA,
+		SDRAM_CLK  => DRAM_CLK,
+		SDRAM_CKE  => DRAM_CKE,
 
-			--SDRAM
-			SDRAM_DQ   => DRAM_DQ,
-			SDRAM_A    => DRAM_ADDR,
-			SDRAM_DQML => DRAM_LDQM,
-			SDRAM_DQMH => DRAM_UDQM,
-			SDRAM_nWE  => DRAM_WE_N,
-			SDRAM_nCAS => DRAM_CAS_N,
-			SDRAM_nRAS => DRAM_RAS_N,
-			SDRAM_nCS  => DRAM_CS_N,
-			SDRAM_BA   => DRAM_BA,
-			SDRAM_CLK  => DRAM_CLK,
-			SDRAM_CKE  => DRAM_CKE,
+		--SRAM
+		SRAM_A		=> SRAM_A,
+		SRAM_Q		=> SRAM_Q,
+		SRAM_WE		=> SRAM_WE,
+		SRAM_OE		=> SRAM_OE,
+		SRAM_UB		=> SRAM_UB,
+		SRAM_LB		=> SRAM_LB,			
 
-			--SRAM
-			SRAM_A		=> SRAM_A,
-			SRAM_Q		=> SRAM_Q,
-			SRAM_WE		=> SRAM_WE,
-			SRAM_OE		=> SRAM_OE,
-			SRAM_UB		=> SRAM_UB,
-			SRAM_LB		=> SRAM_LB,			
+		--UART
+		UART_TX    => PMOD4_D5,
+		UART_RX    => PMOD4_D4,
 
-			--UART
-			UART_TX    => PMOD4_D5,
-			UART_RX    => PMOD4_D4,
+		--SPI
+	--	SPI_DO     => spi_do,
+		SPI_DO     => spi_fromguest,
+		SPI_DO_IN  => sd_miso,
+		SPI_DI     => spi_toguest,
+		SPI_SCK    => spi_clk_int,
+		SPI_SS2    => spi_ss2,
+		SPI_SS3    => spi_ss3,
+		SPI_SS4    => spi_ss4,
+		CONF_DATA0 => conf_data0,
 
-			--SPI
-		--	SPI_DO     => spi_do,
-			SPI_DO     => spi_fromguest,
-			SPI_DO_IN  => sd_miso,
-			SPI_DI     => spi_toguest,
-			SPI_SCK    => spi_clk_int,
-			SPI_SS2    => spi_ss2,
-			SPI_SS3    => spi_ss3,
-			SPI_SS4    => spi_ss4,
-			CONF_DATA0 => conf_data0,
+		--VGA
+		VGA_HS     => vga_hsync,
+		VGA_VS     => vga_vsync,
+		VGA_R      => vga_red(7 downto 2),
+		VGA_G      => vga_green(7 downto 2),
+		VGA_B      => vga_blue(7 downto 2),
 
-			--VGA
-			VGA_HS    => vga_hsync,
-			VGA_VS    => vga_vsync,
-			VGA_R      => vga_red(7 downto 2),
-			VGA_G      => vga_green(7 downto 2),
-			VGA_B      => vga_blue(7 downto 2),
+		--HDMI
+		--RED_x      => vga_x_r,
+		--GREEN_x    => vga_x_g,
+		--BLUE_x     => vga_x_b,
+		--HS_x       => vga_x_hs,
+		--VS_x       => vga_x_vs,
+		--VGA_DE     => vga_de,
+		--VGA_CLK    => vga_clk,
 
-			--HDMI
-			--RED_x      => vga_x_r,
-			--GREEN_x    => vga_x_g,
-			--BLUE_x     => vga_x_b,
-			--HS_x       => vga_x_hs,
-			--VS_x       => vga_x_vs,
-			--VGA_DE     => vga_de,
-			--VGA_CLK    => vga_clk,
+		--JOYSTICKS
+		JOY1 	   => joy1 or intercept_joy,   -- Block joystick when OSD is active
+		JOY2 	   => joy2 or intercept_joy,   -- Block joystick when OSD is active
+		JOY_SELECT => joy_select_o,
 
-			--JOYSTICKS
-			JOY1 	   => joy1 or intercept_joy,   -- Block joystick when OSD is active
-			JOY2 	   => joy2 or intercept_joy,   -- Block joystick when OSD is active
-			JOY_SELECT => joy_select_o,
+		--AUDIO
+		DAC_L      => dac_l,
+		DAC_R      => dac_r,
+		AUDIO_L    => sigma_l,
+		AUDIO_R    => sigma_r,
 
-			--AUDIO
-			DAC_L      => dac_l,
-			DAC_R      => dac_r,
-			AUDIO_L    => sigma_l,
-			AUDIO_R    => sigma_r,
+		OSD_EN	   => osd_en
 
-			OSD_EN	   => osd_en
-
-		);
+	);
 
 
-	-- Pass internal signals to external SPI interface
-	sd_clk <= spi_clk_int;
-	-- spi_do <= sd_miso when spi_ss4='0' else 'Z'; -- to guest
-	-- spi_fromguest <= spi_do;  -- to control CPU
+-- Pass internal signals to external SPI interface
+sd_clk <= spi_clk_int;
+-- spi_do <= sd_miso when spi_ss4='0' else 'Z'; -- to guest
+-- spi_fromguest <= spi_do;  -- to control CPU
 
-	controller : entity work.substitute_mcu
-		generic map(
-			sysclk_frequency => 500,
-	--		SPI_FASTBIT=>3,
-	--		SPI_INTERNALBIT=>2,		--needed if OSD hungs
-	--		SPI_FASTBIT => 0, 		-- Reducing this will make SPI comms faster, for cores which are clocked fast enough.
-	--		SPI_INTERNALBIT => 0, 	-- This will make SPI comms faster, for cores which are clocked fast enough.
-			debug     => false,
-			jtag_uart => false
-		)
-		port map(
-			clk       => CLK_50_buf,					--50 MHz
-			reset_in  => '1',							--reset_in  when 0
-			reset_out => reset_n,						--reset_out when 0
+controller : entity work.substitute_mcu
+	generic map(
+		sysclk_frequency => 500,
+--		SPI_FASTBIT=>3,
+--		SPI_INTERNALBIT=>2,		--needed if OSD hungs
+--		SPI_FASTBIT => 0, 		-- Reducing this will make SPI comms faster, for cores which are clocked fast enough.
+--		SPI_INTERNALBIT => 0, 	-- This will make SPI comms faster, for cores which are clocked fast enough.
+		debug     => false,
+		jtag_uart => false
+	)
+	port map(
+		clk       => CLK_50_buf,					--50 MHz
+		reset_in  => '1',							--reset_in  when 0
+		reset_out => reset_n,						--reset_out when 0
 
-			-- SPI signals
-			spi_miso      => sd_miso,
-			spi_mosi      => sd_mosi,
-			spi_clk       => spi_clk_int,
-			spi_cs        => sd_cs,
-			spi_fromguest => spi_fromguest,
-			spi_toguest   => spi_toguest,
-			spi_ss2       => spi_ss2,
-			spi_ss3       => spi_ss3,
-			spi_ss4       => spi_ss4,
-			conf_data0    => conf_data0,
+		-- SPI signals
+		spi_miso      => sd_miso,
+		spi_mosi      => sd_mosi,
+		spi_clk       => spi_clk_int,
+		spi_cs        => sd_cs,
+		spi_fromguest => spi_fromguest,
+		spi_toguest   => spi_toguest,
+		spi_ss2       => spi_ss2,
+		spi_ss3       => spi_ss3,
+		spi_ss4       => spi_ss4,
+		conf_data0    => conf_data0,
 
-			-- PS/2 signals
-			ps2k_clk_in  => ps2_keyboard_clk_in,
-			ps2k_dat_in  => ps2_keyboard_dat_in,
-			ps2k_clk_out => ps2_keyboard_clk_out,
-			ps2k_dat_out => ps2_keyboard_dat_out,
-			ps2m_clk_in  => ps2_mouse_clk_in,
-			ps2m_dat_in  => ps2_mouse_dat_in,
-			ps2m_clk_out => ps2_mouse_clk_out,
-			ps2m_dat_out => ps2_mouse_dat_out,
+		-- PS/2 signals
+		ps2k_clk_in  => ps2_keyboard_clk_in,
+		ps2k_dat_in  => ps2_keyboard_dat_in,
+		ps2k_clk_out => ps2_keyboard_clk_out,
+		ps2k_dat_out => ps2_keyboard_dat_out,
+		ps2m_clk_in  => ps2_mouse_clk_in,
+		ps2m_dat_in  => ps2_mouse_dat_in,
+		ps2m_clk_out => ps2_mouse_clk_out,
+		ps2m_dat_out => ps2_mouse_dat_out,
 
-			-- Buttons
-			buttons => (others => '1'),	-- 0 = opens OSD
+		-- Buttons
+		buttons => (others => '1'),	-- 0 = opens OSD
 
-			-- Joysticks
-			joy1 => joya,
-			joy2 => joyb,
+		-- Joysticks
+		joy1 => joya,
+		joy2 => joyb,
 
-			-- UART
-			rxd  => rs232_rxd,
-			txd  => rs232_txd,
-			--
-			intercept => intercept
-		);
+		-- UART
+		rxd  => rs232_rxd,
+		txd  => rs232_txd,
+		--
+		intercept => intercept
+	);
 
-	LED5 <= not act_led;
+LED5 <= not act_led;
 
 end rtl;
